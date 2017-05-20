796d1dc686f3fde65da8e50061f02929119349cd
timsgardner <timsgardner@gmail.com>
2017-05-19 23:15:48 -0400

    modifying sigs of game functions for pass-keys arcadia branch

--- Game/Assets/game/people.clj
+++ Game/Assets/game/people.clj
@@ -11,8 +11,10 @@
 (def name-tag-prefab
   (Resources/Load "Name Tag"))
 
-(defn start [gobj]
-  (server/start-updates!))
+(defn start
+  ([gobj _] (start gobj))
+  ([gobj]
+   (server/start-updates!)))
 
 (defn controller->locomotion
   [{:strs [dpad-up dpad-left dpad-right button-a]}]
@@ -34,35 +36,37 @@
         (v3+ (world->canvas (.. gobj transform position))
              (v3 0 50 0))))
 
-(defn populate [gobj]
-  (let [players @server/players
-        controllers @server/controllers
-        canvas (object-named "Canvas")
-        ids (-> players keys set)]
-    ;; add new children
-    (doseq [[id {:strs [name number]}] players]
-      (when-not (.. gobj transform (Find id))
-        (let [person (instantiate person-prefab)
-              name-tag (instantiate name-tag-prefab)]
-          (set! (.name person) id)
-          (child+ gobj person)
-          (set! (.name name-tag) id)
-          (set! (.text (cmpt name-tag Text)) (str "#" number))
-          (child+ canvas name-tag)
-          (role+ person :name-tag
-                 {:state name-tag
-                  :update #'snap-name-tag}))))
-    ;; remove missing children
-    (doseq [child (.. gobj transform)]
-      (when-not (ids (.name child))
-        (destroy (.gameObject child))
-        (destroy (.. canvas transform (Find )))
-        ))
-    ;; controll players
-    (doseq [[id controller] controllers]
-      (when-let [child (.. gobj transform (Find id))]
-        (set-state! child :locomotion
-                    (controller->locomotion controller))))))
+(defn populate
+  ([gobj _] (populate gobj))
+  ([gobj]
+   (let [players @server/players
+         controllers @server/controllers
+         canvas (object-named "Canvas")
+         ids (-> players keys set)]
+     ;; add new children
+     (doseq [[id {:strs [name number]}] players]
+       (when-not (.. gobj transform (Find id))
+         (let [person (instantiate person-prefab)
+               name-tag (instantiate name-tag-prefab)]
+           (set! (.name person) id)
+           (child+ gobj person)
+           (set! (.name name-tag) id)
+           (set! (.text (cmpt name-tag Text)) (str "#" number))
+           (child+ canvas name-tag)
+           (role+ person :name-tag
+             {:state name-tag
+              :update #'snap-name-tag}))))
+     ;; remove missing children
+     (doseq [child (.. gobj transform)]
+       (when-not (ids (.name child))
+         (destroy (.gameObject child))
+         (destroy (.. canvas transform (Find )))
+         ))
+     ;; controll players
+     (doseq [[id controller] controllers]
+       (when-let [child (.. gobj transform (Find id))]
+         (set-state! child :locomotion
+           (controller->locomotion controller)))))))
 
 (comment
   (role+ (object-named "People") :people
