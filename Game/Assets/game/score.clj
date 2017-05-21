eecddaa20d8adc16718a438cef031798b89a29c1
Ramsey Nasser <ram@nas.sr>
2017-05-21 15:09:20 -0400

    add score zone animation

--- Game/Assets/game/score.clj
+++ Game/Assets/game/score.clj
@@ -1,7 +1,8 @@
 (ns game.score
-  (:use arcadia.core)
+  (:use arcadia.core
+        arcadia.linear)
   (:require [clojure.string :as string])
-  (:import [UnityEngine Time UI.Text]))
+  (:import [UnityEngine Time UI.Text Vector3 Quaternion]))
 
 (def green (atom 0))
 (def purple (atom 0))
@@ -22,6 +23,45 @@
                  :purple purple})
            inc)))
 
+(defn jelly-start [gobj k]
+  (set-state!
+    gobj k
+    (merge (state gobj k)
+           {:position (.. gobj transform localPosition)
+            :rotation (.. gobj transform localRotation)
+            :scale (.. gobj transform localScale)})))
+
+(defn jelly-resize [gobj k]
+  (let [{:keys [speed position rotation scale]}
+        (state gobj k)]
+    (set! (.. gobj transform localPosition)
+          (Vector3/Lerp (.. gobj transform localPosition)
+                        position
+                        speed))
+    (set! (.. gobj transform localRotation)
+          (Quaternion/Slerp (.. gobj transform localRotation)
+                            rotation
+                            speed))
+    (set! (.. gobj transform localScale)
+          (Vector3/Lerp (.. gobj transform localScale)
+                        scale
+                        speed))))
+
+(defn jelly-animate! [gobj spec]
+  (set-state!
+    gobj :jelly
+    (merge (state gobj :jelly)
+           spec)))
+
+(def jelly
+  {:state {:position nil
+           :rotation nil
+           :scale nil
+           :speed 0.5}
+   :start #'jelly-start
+   :update #'jelly-resize})
+
 (comment 
+  (role+ (object-named "Purple Zone") :jelly jelly)
   (hook+ (object-named "Green Zone") :on-trigger-enter :green #'on-trigger)
   (hook+ (object-named "Purple Zone") :on-trigger-enter :purple #'on-trigger))
